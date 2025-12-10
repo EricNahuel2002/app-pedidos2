902648734daed8c66bd1cf84c651818d21f900fd
EricNahuel2002 <ericaquino2002@gmail.com>
2025-12-10 15:40:56 -0300

    base de tests

---
+++ back/tests/ApiGateway.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiGateway.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ back/tests/Ordenes/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordenes
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
